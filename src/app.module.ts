82f3227173e27cc07cd4f15843671b1091e6ef68
guilhiz <guilhermevilelasobral@gmail.com>
2023-06-12 14:29:40 -0300

    feat: initial configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          loggingMiddleware({
+            logger: new Logger('PrismaMiddleware'),
+            logLevel: 'log',
+          }),
+        ],
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  app.setGlobalPrefix('api', { exclude: ['/'] });
+
+  const prismaService: PrismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
+  console.info(`server running on ${PORT}`);
 }
 bootstrap();
